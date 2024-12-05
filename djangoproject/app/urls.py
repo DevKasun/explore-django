14d4e2b14ef56a67d42f4f46677239e967bfc8e8
Kasun Lakshitha <kasun2014@outlook.com>
2024-12-05 16:28:31 +0530

    Refactor article creation to use class-based view and remove legacy form

--- djangoproject/app/urls.py
+++ djangoproject/app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from app.views import home, create_article
+from app.views import home, ArticleCreateView
 
 urlpatterns = [
     path('', home),
-    path("article/create/", create_article, name="create_article")
+    path("article/create/", ArticleCreateView.as_view(), name="create_article")
 ]
--- djangoproject/app/views.py
+++ djangoproject/app/views.py
@@ -1,28 +1,39 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
+from django.urls import reverse_lazy
 from app.models import Article
-from app.forms import CreateArticleForm
+from django.views.generic import CreateView
 
 def home(request):
     article = Article.objects.all()
     return render(request, 'app/home.html', {'article': article})
 
-def create_article(request):
-    if request.method == "post":
-        # Data is submitted
-        form = CreateArticleForm(request.POST)
-        if form.is_valid():
-            form_data = form.cleaned_data
-            new_article = Article(
-                title = form_data['title'],
-                status = form_data['status'],
-                content = form_data['content'],
-                word_count = form_data['word_count'],
-                twitter_post = form_data['twitter_post']
-            )
+
+#We can create same form using class based view like this
+
+class ArticleCreateView(CreateView):
+    template_name = 'app/article_create.html'
+    model = Article
+    fields = ['title', 'status', 'content', 'word_column', 'twitter_post']
+    success_url = reverse_lazy("home")
+
+
+# def create_article(request):
+#     if request.method == "post":
+#         # Data is submitted
+#         form = CreateArticleForm(request.POST)
+#         if form.is_valid():
+#             form_data = form.cleaned_data
+#             new_article = Article(
+#                 title = form_data['title'],
+#                 status = form_data['status'],
+#                 content = form_data['content'],
+#                 word_count = form_data['word_count'],
+#                 twitter_post = form_data['twitter_post']
+#             )
             
-            new_article.save()
+#             new_article.save()
             
-            return redirect("home")
-    else: 
-        form = CreateArticleForm()
-    return render(request, 'app/article_create.html', {'form': form})
+#             return redirect("home")
+#     else: 
+#         form = CreateArticleForm()
+#     return render(request, 'app/article_create.html', {'form': form})
