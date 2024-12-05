73fe935bd7459eefd9a5a05011820141b4eb671e
Kasun Lakshitha <kasun2014@outlook.com>
2024-12-05 14:05:16 +0530

    Add article creation functionality with form and template (function based form)

--- djangoproject/app/urls.py
+++ djangoproject/app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from app.views import home
+from app.views import home, create_article
 
 urlpatterns = [
     path('', home),
+    path("article/create/", create_article, name="create_article")
 ]
--- djangoproject/app/views.py
+++ djangoproject/app/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from app.models import Article
+from app.forms import CreateArticleForm
 
 def home(request):
     article = Article.objects.all()
     return render(request, 'app/home.html', {'article': article})
+
+def create_article(request):
+    if request.method == "post":
+        # Data is submitted
+        form = CreateArticleForm(request.POST)
+        if form.is_valid():
+            form_data = form.cleaned_data
+            new_article = Article(
+                title = form_data['title'],
+                status = form_data['status'],
+                content = form_data['content'],
+                word_count = form_data['word_count'],
+                twitter_post = form_data['twitter_post']
+            )
+            
+            new_article.save()
+            
+            return redirect("home")
+    else: 
+        form = CreateArticleForm()
+    return render(request, 'app/article_create.html', {'form': form})
