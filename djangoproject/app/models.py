e6a1c549517414fe139eac825f1fa9b3e48e6abc
Kasun Lakshitha <kasun2014@outlook.com>
2024-12-09 15:02:16 +0530

    Update article model to calculate word count dynamically, modify migration for word_column, and enhance templates for article creation, update, and deletion

--- djangoproject/app/models.py
+++ djangoproject/app/models.py
@@ -1,3 +1,4 @@
+import re
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -14,8 +15,13 @@ class UserProfile(AbstractUser):
 class Article(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField(blank=True, default="")
-    word_column = models.IntegerField()
+    word_column = models.IntegerField(blank=True, default="")
     twitter_post = models.TextField(blank=True, default="")
     status = models.CharField(max_length=20, choices=ARTICLE_STATUS, default="draft")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    
+    def save(self, *args, **kwargs):
+            text = re.sub(r"<[^>]*>=", "", self.content).replace("&nbsp;", " ")
+            self.word_column = len(re.findall(r"\b\w+\b", text))
+            super().save(*args, **kwargs)
--- djangoproject/app/urls.py
+++ djangoproject/app/urls.py
@@ -8,7 +8,7 @@ from app.views import (
 
 urlpatterns = [
     path("", ArticleListView.as_view(), name="home"),
-    path("create/", ArticleCreateView.as_view(), name="article_create"),
+    path("create/", ArticleCreateView.as_view(), name="create_article"),
     path("<int:pk>/update/", ArticleUpdateView.as_view(), name="update_article"),
     path("<int:pk>/delete/", ArticleDeleteView.as_view(), name="delete_article"),
 ]
