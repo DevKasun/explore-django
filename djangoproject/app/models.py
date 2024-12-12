af6356f88d77f1f04e94b2d265a4ab26167e5900
Kasun Lakshitha <kasun2014@outlook.com>
2024-12-12 22:52:57 +0530

    Add localization support and update Article model fields for better readability

--- djangoproject/app/models.py
+++ djangoproject/app/models.py
@@ -1,6 +1,7 @@
 import re
 from django.db import models
 from django.conf import settings
+from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import AbstractUser
 
 ARTICLE_STATUS = (
@@ -14,14 +15,24 @@ class UserProfile(AbstractUser):
     pass
 
 class Article(models.Model):
-    title = models.CharField(max_length=100)
-    content = models.TextField(blank=True, default="")
-    word_column = models.IntegerField(blank=True, default="")
-    twitter_post = models.TextField(blank=True, default="")
-    status = models.CharField(max_length=20, choices=ARTICLE_STATUS, default="draft")
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="articles")
+    title = models.CharField(_("title"),max_length=100)
+    content = models.TextField(_("content"),blank=True, default="")
+    word_column = models.IntegerField(_("word count"),blank=True, default="")
+    twitter_post = models.TextField(_("twitter post"),blank=True, default="")
+    status = models.CharField(
+        _("status"),
+        max_length=20, 
+        choices=ARTICLE_STATUS, 
+        default="draft"
+    )
+    created_at = models.DateTimeField(_("created at"),auto_now_add=True)
+    updated_at = models.DateTimeField(_("updated at"),auto_now=True)
+    creator = models.ForeignKey(
+        settings.AUTH_USER_MODEL, 
+        on_delete=models.CASCADE, 
+        related_name="articles", 
+        verbose_name=_("creator")
+    )
     
     def save(self, *args, **kwargs):
             text = re.sub(r"<[^>]*>=", "", self.content).replace("&nbsp;", " ")
--- djangoproject/djangoproject/settings.py
+++ djangoproject/djangoproject/settings.py
@@ -140,13 +140,17 @@ ACCOUNT_EMAIL_VERIFICATION = "none"
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_TZ = True
+
+locale_paths = [
+    BASE_DIR / "locale",
+]
 
 
 # Static files (CSS, JavaScript, Images)
