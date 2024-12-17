7e2866037efac57b3b6818a0bea617d66b80f9ea
Kasun Lakshitha <kasun2014@outlook.com>
2024-12-17 11:47:05 +0530

    Add Tailwind CSS configuration and update templates for styling

--- djangoproject/djangoproject/settings.py
+++ djangoproject/djangoproject/settings.py
@@ -160,6 +160,8 @@ LOCALE_PATHS = [
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
